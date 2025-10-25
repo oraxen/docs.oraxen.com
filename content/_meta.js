ddd4626f5928f51e1f98e01e1462620b9971a4bb
Thomas Marchand <thomas.marchand@tuta.io>
2025-10-25 17:26:45 +0800

    fix: clean

--- content/_meta.js
+++ content/_meta.js
@@ -1,11 +1,10 @@
 export default {
-  index: 'Getting started',
-  faq: 'Frequently Asked Questions',
-  usage: 'Usage',
-  configuration: 'Configuration',
-  mechanics: 'Mechanics',
-  addons: 'Addons',
-  compatibility: 'Compatibility',
-  vendors: 'Vendors',
-  developers: 'Developers'
-}
+  index: "Getting started",
+  faq: "Frequently Asked Questions",
+  usage: "Usage",
+  configuration: "Configuration",
+  mechanics: "Mechanics",
+  compatibility: "Compatibility",
+  vendors: "Vendors",
+  developers: "Developers",
+};
