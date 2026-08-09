4dab98f5ab67f3b74d0eddae46b5e5486d74b833
pxlarified <216089890+pxlarified@users.noreply.github.com>
2026-08-09 14:30:06 +0200

    Add documentation search

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -1,4 +1,5 @@
 import OriginalNavbar from '@theme-original/Navbar';
+import SearchBar from '@theme/SearchBar';
 
 export default function Navbar() {
   // Docusaurus' table-of-contents positioning expects a .navbar element.
@@ -6,6 +7,9 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar oraxen-navbar-anchor" aria-hidden="true" />
+      <div className="oraxen-desktop-search">
+        <SearchBar />
+      </div>
       <div className="oraxen-mobile-navbar">
         <OriginalNavbar />
       </div>
