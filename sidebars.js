b475abe48545713bf7f0db2de2c8f7515590fd17
pxlarified <jan.kramer@mizius.com>
2026-09-03 20:54:40 +0200

    Add MineResetLite compatibility documentation

--- sidebars.js
+++ sidebars.js
@@ -18,7 +18,7 @@ const sidebars = {docsSidebar: [
     'usage/commands', 'usage/recipes', 'usage/merging', 'usage/hosting', 'usage/settings',
   ]},
   {type: 'category', label: '🤝 Compatibility', link: {type: 'doc', id: 'compatibility/index'}, items: [
-    'compatibility/placeholderapi', 'compatibility/skript', 'compatibility/viaversion', 'compatibility/packlayer', 'compatibility/modelengine', 'compatibility/mythichud', 'compatibility/mythiccrucible', 'compatibility/mythicmobs', 'compatibility/mmoitems', 'compatibility/betterhud', 'compatibility/blocklocker', 'compatibility/ecoitems', 'compatibility/executableitems', 'compatibility/worldedit',
+    'compatibility/placeholderapi', 'compatibility/skript', 'compatibility/viaversion', 'compatibility/packlayer', 'compatibility/modelengine', 'compatibility/mythichud', 'compatibility/mythiccrucible', 'compatibility/mythicmobs', 'compatibility/mmoitems', 'compatibility/betterhud', 'compatibility/blocklocker', 'compatibility/ecoitems', 'compatibility/executableitems', 'compatibility/worldedit', 'compatibility/mineresetlite',
     {type: 'category', label: '🧱 Bedrock', link: {type: 'doc', id: 'compatibility/bedrock/index'}, items: [
       {type: 'category', label: '🪶 BedrockGen', link: {type: 'doc', id: 'compatibility/bedrock/bedrockgen/index'}, items: ['compatibility/bedrock/bedrockgen/usage']},
     ]},
